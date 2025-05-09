3e0b52f9a1f5ffacaa5203574285dede2e15e79d
AeliniaAPCSP <enquista@wlhs.wlwv.k12.or.us>
2025-05-09 15:16:44 -0700

    Update Blackjack.java

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -2,34 +2,36 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Random;
+import javax.print.DocFlavor;
 import javax.swing.*;
 import java.util.Collections;
+import java.util.concurrent.ExecutionException;
 
 public class Blackjack {
-    private class Card{
+    private class Card {
         private String value;
         private String suit;
 
-        public Card(String value, String suit){
+        public Card(String value, String suit) {
             this.value = value;
             this.suit = suit;
         }
 
         @Override
-        public String toString(){
-            return this.value +"-"+this.suit;
+        public String toString() {
+            return this.value + "-" + this.suit;
         }
 
-        public int getValue(){
-            if("KQJ".contains(value)){
+        public int getValue() {
+            if ("KQJ".contains(value)) {
                 return 10;
-            } else if(value.equals("A")){
+            } else if (value.equals("A")) {
                 return 11;
             }
             return Integer.parseInt(this.value);
         }
 
-        public boolean isAce(){
+        public boolean isAce() {
             return value.equals("A");
         }
 
@@ -53,9 +55,8 @@ public class Blackjack {
 
     //window
     int boardWidth = 600;
-    int boardHeight = boardWidth;
-
-    int cardWidth = 110; //1:1.4 ratio 
+    int boardHeight = 600;
+    int cardWidth = 110; //1:1.4 ratio
     int cardHeight = 154;
 
     JFrame frame = new JFrame("Blackjack");
@@ -64,48 +65,68 @@ public class Blackjack {
         public void paintComponent(Graphics g){
             super.paintComponent(g);
 
-
             try{
-            //draw hidden card
-            Image hiddenCardImg = new ImageIcon(getClass().getResource("./cards/BACK.png")).getImage();
-            g.drawImage(hiddenCardImg,20,20,cardWidth,cardHeight,null);
+                //draw hidden card
+                Image hiddenCardImg = new ImageIcon(getClass().getResource("./cards/BACK.png")).getImage();
+                g.drawImage(hiddenCardImg,20,20,cardWidth,cardHeight,null);
 
-            //draw dealer's hand
-            for(int i = 0; i<dealerHand.size();i++){
-                Card card = dealerHand.get(i);
-                Image cardImg = new ImageIcon(getClass().getResource(card.getImagePath())).getImage();
-                g.drawImage(cardImg,cardWidth+ 25,20,cardWidth,cardHeight,null);
-            }
+                //draw dealer's hand
+                for(int i = 0; i < dealerHand.size();i++){
+                    Card card = dealerHand.get(i);
+                    Image cardImg = new ImageIcon(getClass().getResource(card.getImagePath())).getImage();
+                    g.drawImage(cardImg,cardWidth + 25,20, cardWidth, cardHeight,null);
+                }
+
+                //draw player's hand
+                for(int i = 0; i < playerHand.size();i++){
+                    Card card = playerHand.get(i);
+                    Image cardImg = new ImageIcon(getClass().getResource(card.getImagePath())).getImage();
+                    g.drawImage(cardImg,20 + (cardWidth + 5)*i,320, cardWidth, cardHeight,null);
+                }
             }
             catch (Exception e){
                 e.printStackTrace();
             }
         }
     };
+
     JPanel buttonPanel = new JPanel();
     JButton hitButton = new JButton("Hit");
     JButton stayButton = new JButton("Stay");
 
-    public Blackjack(){
+    public Blackjack() {
         startGame();
 
         frame.setVisible(true);
-        frame.setSize(boardWidth,boardHeight);
+        frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         gamePanel.setLayout(new BorderLayout());
-        gamePanel.setBackground(new Color(53,101,77));
+        gamePanel.setBackground(new Color(55, 101, 77));
         frame.add(gamePanel);
 
         hitButton.setFocusable(false);
         buttonPanel.add(hitButton);
         stayButton.setFocusable(false);
         buttonPanel.add(stayButton);
-        frame.add(buttonPanel,BorderLayout.SOUTH);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
 
+        hitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Card card = deck.remove(deck.size()-1);
+                playerSum += card.getValue();
+                playerAceCount += card.isAce()? 1 : 0;
+                playerHand.add(card);
+                gamePanel.repaint();
+                if(reducePlayerAce() > 21) {
+                    hitButton.setEnabled(false);
+                } //I was at 46 minutes in the video
+            }
+        });
 
+        gamePanel.repaint();
     }
 
     public void startGame(){
